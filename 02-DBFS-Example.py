2d6cc30df7958bf7e5846b84d5f804061258416c
Varpe Jalindar <jalindarvarpe2015@gmail.com>
2024-02-03 23:16:14 +0530

    Adding new changes

---
+++ 02-DBFS-Example.py
@@ -0,0 +1,41 @@
+# File location and type
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+spark = SparkSession.builder.appName("example").getOrCreate()
+
+
+
+file_location = "/FileStore/tables/mydata/asl.csv"
+file_type = "csv"
+
+# CSV options
+infer_schema = "false"
+first_row_is_header = "false"
+delimiter = ","
+
+# The applied options are for CSV files. For other file types, these will be ignored.
+df = spark.read.format(file_type) \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load(file_location)
+
+df.show()
+
+
+
+# Create a view or table
+
+temp_table_name = "asl_csv"
+
+df.createOrReplaceTempView(temp_table_name)
+
+
+# With this registered as a temp view, it will only be available to this particular notebook. If you'd like other users to be able to query this table, you can also create a table from the DataFrame.
+# Once saved, this table will persist across cluster restarts as well as allow various users across different notebooks to query this data.
+# To do so, choose your table name and uncomment the bottom line.
+
+permanent_table_name = "asl_csv"
+
+# df.write.format("parquet").saveAsTable(permanent_table_name)
